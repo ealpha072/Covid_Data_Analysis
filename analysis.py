317518f4210974b168be607950bff78ee6236918
ealpha072 <ealpha072@gmail.com>
2022-09-08 21:19:32 +0300

    More than one country option

--- analysis.py
+++ analysis.py
@@ -1,3 +1,4 @@
+from array import array
 import pandas as pd
 import numpy as np
 import os
@@ -7,26 +8,32 @@ import logging
 data = pd.read_csv('clean_data.csv')
 
 #generates country data
-def get_country_data(country, directory_name, data = ''):
-    country_name = country.lower()
+def get_country_data(country:list, directory_name:str, data = ''):
+    country_name = [i.capitalize() for i in country]
+    print(country_name)
     path = os.getcwd()
     new_dir = os.path.join(path, directory_name)
+    status = False
     if not os.path.exists(directory_name):
         os.mkdir(new_dir)
     else:
         print('Directory Exists, data will be saved in existing directory')
-        
-    options_to_check = [i for i in list(data['location']) if i[0].lower() == country_name[0]][0]
     
-    if len(options_to_check) == 0:
+    for i in country_name:
+        if i not in list(data['location']):
+            print('Unable to find the specified location')
+        else:
+            status = True
+            print('Found location')
+    print(status)
+    if not status:
         print('Error, you gave an invalid country name')
-        print(options_to_check)
     else:
-        country_data = data.loc[data['location'] == country]
-        try:
-            #
-            country_data.to_csv(new_dir + country_name + 'covid_data.csv')
-            print('Data generated successsfully')
-        except Exception as e:
-            logging.error('Failed to generate data: '+ str(e))
-        return country_data
+        for i in country_name:
+            country_data = data.loc[data['location'] == i]
+            try:
+                country_data.to_csv(i.lower() + '_covid_data.csv')
+                print('Data generated successsfully')
+            except Exception as e:
+                logging.error('Failed to generate data: '+ str(e))
+            return country_data
